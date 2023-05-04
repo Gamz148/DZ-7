0dd891fca79b8db712c82dfd2693c6f1e039f35e
Oleg <gamz_1@mail.ru>
2023-05-04 21:14:41 +0200

    первая фиксация

---
+++ 1/Program.cs
@@ -0,0 +1,63 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+
+// Random rnd = new Random();
+// int[] my_array = new int[rnd.Next(3,8)];
+// int[] new_array = new int[my_array.Length];
+
+// void RandArray(int[] arr)
+// {
+//     for (int i =0; i < arr.Length; i++)
+//     {
+//         int add = rnd.Next(1, 10);
+//         arr[i] = add;
+//     }
+// }
+
+// string PrintArray(int[] arr)
+// {
+//     string res = "";
+//     for (int i =0; i < (arr.Length)-1; i++)
+//     {
+//         res += Convert.ToString(arr[i]);
+//         res += ", ";
+//     }
+//     res += Convert.ToString(arr[arr.Length -1]);
+//     return res;
+// }
+
+System.Console.WriteLine("введите количество строк m ");
+int M = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите количество столбцов n ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void CreateAndPrintMatrix(int a, int b)
+{
+    Random rnd = new Random();
+    double[,] arr = new double[a, b];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j=0; j < arr.GetLength(1); j++)
+        {
+            //double add = rnd.Next(1, 10);
+            arr[i,j] = rnd.NextDouble();
+        }
+    }
+    for (int i =0; i < arr.GetLength(0); i++)
+    {
+        for (int j =0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+CreateAndPrintMatrix(M, N);
+
+
---
+++ 3/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+    int m = 3; // число строк
+    int n = 4; // число столбцов
+
+    // создаем пустой массив заданного размера
+    int[,] array = new int[m, n];
+
+    // заполняем массив случайными вещественными числами
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = rand.Next(1, 10);
+        }
+    }
+
+    // выводим массив на экран
+    Console.WriteLine("Массив:");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+         Console.WriteLine();
+    }
+
+
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int j = 0; j < columns; j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            sum += array[i, j];
+        }
+        double average = sum / rows;
+        Console.WriteLine($"Среднее арифметическое элементов в столбце {j}: {average}");
+    }
+
